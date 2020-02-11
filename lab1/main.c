7b8b3473a4ec6e33b3c236d176a15e7055585a6a
MrPipiKado <ammamm23102310@gmail.com>
2020-02-11 22:30:11 +0200

    lab_2

--- lab1/main.c
+++ lab1/main.c
@@ -1,29 +1,41 @@
 #include <stdio.h>
 #include <math.h>
 
-float function(float x)
-{
-    return sqrtf(64.0f-x*x)*log2f(x)-sinf(2*x);
+double function(double x)
+{//задана функція
+    return sqrt(64.0f-x*x)*log2(x)-sin(2*x);
+}
+
+double func_poh_1(double x)
+{//перша похідна
+    return (-log2(x)/sqrt(64.0-x*x)) + (sqrt(64.0-x*x)/(x*M_LN2)) - 2*cos(2*x);
+}
+
+double func_poh_2(double x)
+{//друга похідна
+    return 4*sin(2*x) -(sqrt(64-x*x)/(x*x*M_LN2)) -
+            ((x*x)/(sqrt((64.0-x*x)*(64.0-x*x)*(64.0-x*x))*log2(x))) -
+            (2/(sqrt(64.0-x*x)*M_LN2)) - (1/(sqrt(64.0-x*x)*log2(x)));
 }
 
 int main()
 {
-    float x, a, b, eps;
+    double x, x0, a, b, eps;
     int count = 0;
     do
     {
         printf("ODZ x є (0;8]\n");
         printf("Enter a:");
-        scanf("%f", &a);
+        scanf("%lf", &a);
         printf("Enter b:");
-        scanf("%f", &b);
+        scanf("%lf", &b);
         printf("Enter eps:");
-        scanf("%f", &eps);
+        scanf("%lf", &eps);
 
     }while(a<0 || b<0 || a>8 || b>8 || //перевір ОДЗ
-            (function(a)*function(b))>0 || (fabsf(a-b)<eps));
+            (function(a)*function(b))>0 || (fabs(a-b)<eps));
 
-    while(fabsf(a-b)>eps)
+    while(fabs(a-b)>eps)
     {
         ++count;
         x = (a+b)/2;
@@ -34,18 +46,20 @@ int main()
         if(function(b)*function(x)>0)
             b=x;
     }
-    printf("x = %f, iteration = %d, precision = %f\n", x, count, eps);
+    printf("x = %lf, iteration = %d, precision = %20.19lf\n", x, count, eps);
 
     do
     {
+        printf("ODZ x є (0;8]\n");
         printf("Enter a:");
-        scanf("%f", &a);
+        scanf("%lf", &a);
         printf("Enter b:");
-        scanf("%f", &b);
+        scanf("%lf", &b);
         printf("Enter eps:");
-        scanf("%f", &eps);
-    }while((function(a)*function(b))>0 || (fabsf(a-b)<eps));
+        scanf("%lf", &eps);
 
+    }while(a<0 || b<0 || a>8 || b>8 || //перевір ОДЗ
+           (function(a)*function(b))>0 || (fabs(a-b)<eps));
 
 
 
